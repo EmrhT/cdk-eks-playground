4fc4cf98ca3e644a187341f67590eb38120ffa33
emrht <emrhtfn@gmail.com>
2023-12-04 11:02:24 +0300

    Initial commit

---
+++ test/playground-eks.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PlaygroundEks from '../lib/playground-eks-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/playground-eks-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PlaygroundEks.PlaygroundEksStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
