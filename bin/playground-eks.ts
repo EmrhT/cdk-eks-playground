f8def6a64363141c575fcc0ce0fc980e183a8fda
emrht <emrhtfn@gmail.com>
2024-01-18 23:24:34 +0300

    small fix

--- bin/playground-eks.ts
+++ bin/playground-eks.ts
@@ -5,7 +5,9 @@ import { PlaygroundEksStack } from '../lib/playground-eks-stack';
 import { DataStack } from '../lib/DataStack';
 
 const app = new cdk.App();
-const dataStack = new DataStack(app, 'DataStack');
+const dataStack = new DataStack(app, 'DataStack', {
+});
+
 new PlaygroundEksStack(app, 'PlaygroundEksStack', {
   playgroundTable: dataStack.playgroundTable,
   playgroundBucket: dataStack.playgroundBucket,
